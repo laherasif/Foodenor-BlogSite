16c507fa99e8ff842fb4ea436da4a408a142dacd
laherasif <laherasif@gmail.com>
2021-05-04 23:03:26 +0500

    deleted files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import 'fontawesome-4.7'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
+import { Link } from 'gatsby'
 export default function Home() {
   return (
     <div>
@@ -69,7 +70,7 @@ export default function Home() {
                     </div>
                     <div className="col-md-5">
                       <div className="dicover-imagecard card">
-                        <img className="card-img-top" src="http://placeimg.com/640/480/nature" alt="Card image cap"  />
+                        <img className="card-img-top" src="http://placeimg.com/640/480/nature" alt="Card image cap" />
                         <div className="card-body">
                           <div className="title" >
                             <h5 className="card-title ">Blog Heading</h5>
@@ -87,22 +88,26 @@ export default function Home() {
                       </div>
                     </div>
                     <div className="col-md-4">
-                      <div className="card mb-3">
-                        <img className="card-img-top" src="http://placeimg.com/640/480/nature" alt="Card image cap" style={{ height: '214px' }} />
+                      <div className="card">
                         <div className="card-body">
-                          <div className="title" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                            <h5 className="card-title ">Card title</h5>
-                            <div className="stars " >
-                              <span className="fa fa-star checked"></span>
-                              <span className="fa fa-star checked"></span>
-                              <span className="fa fa-star checked"></span>
-                              <span className="fa fa-star"></span>
-                              <span className="fa fa-star"></span>
+                          <ul id="review-card">
+                            <li>
+                            <div className="card-style">
+                              dadkasjh
                             </div>
-                          </div>
-
-                          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            </li>
+                            <li>
+                            <div className="card-style">
+                              dadkasjh
+                            </div>
+                            </li>
+                            <li>
+                            <div className="card-style">
+                              dadkasjh
+                            </div>
+                            </li>
+                          </ul>
+                        
                         </div>
                       </div>
                     </div>
@@ -110,9 +115,68 @@ export default function Home() {
                   </div>
                 </div>
               </section>
+
+
             </div>
           </div>
         </div>
+
+        <footer id="footer" >
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-sm-7">
+                <div className="footer__about">
+                  <div className="footer__logo" style={{ display: 'flex' }}>
+                    <img src="images/FoodieNor logo design .png" alt="food_logo" />
+                    <h6 >FOOFIENOR </h6>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-3 col-sm-5">
+                <div className="footer__widget">
+                  <h6>CATAGORIES</h6>
+                  <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">About</Link></li>
+                    <li><Link to="/">Blogs</Link></li>
+                    <li><Link to="/">Contact</Link></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-3 col-sm-4">
+                <div className="footer__widget">
+                  <h6>Tags Used</h6>
+                  <ul>
+                    <li><Link to="/">My Account</Link></li>
+                    <li><Link to="/">Orders Tracking</Link></li>
+                    <li><Link to="/">Checkout</Link></li>
+                    <li><Link to="/">Wishlist</Link></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-8 col-sm-8">
+                <div id="footer__newslatter">
+                  <h6>NEWSLETTER</h6>
+                  <form action="#">
+                    <input type="text" placeholder="Email Us" />
+                    <button type="submit" className="site-btn"><i className="fa fa-long-arrow-right"></i></button>
+                  </form>
+                  <div id="footer__social">
+                    <Link to="/" className="links"><i className="fa fa-linkedin" /></Link>
+                    <Link to="/" className="links"><i className="fa fa-twitter" /></Link>
+                    <Link to="/" className="links"><i className="fa fa-behance" /></Link>
+                    <Link to="/" className="links"><i className="fa fa-instagram" /></Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </footer>
       </div>
 
 
