657ed8f6d4e46a2947648230e66a6d0d5e1f736d
laherasif <laherasif@gmail.com>
2021-05-04 13:20:25 +0500

    seeting up leanding page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,12 @@ export default function Home() {
                 <div className="section-heading">
 
                   <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                       <div className="heading">
                         <p>
                           Sub Title
-                     <br></br>
-                          <h2>Heading</h2>
-                        </p>
+                          </p>
+                        <h2>Heading</h2>
                         <p className="loram">
                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio congue egestas ut vitae felis, pellentesque
                       </p>
@@ -28,7 +27,7 @@ export default function Home() {
 
                       </div>
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-md-7">
                       <div className="heading-logo">
                         <img src="images/FoodieNor logo design .png" alt="food_logo" />
                       </div>
@@ -36,9 +35,9 @@ export default function Home() {
                     <div className="col-md-2">
                       <div className="heading-buttons">
                         <ul>
-                          <li><button type="button" className="btn btn-outline-primary">Primary</button></li>
-                          <li><button type="button" className="btn btn-outline-primary">Primary</button></li>
-                          <li><button type="button" className="btn btn-outline-primary">Primary</button></li>
+                          <li><button type="button" className="btn btn-outline-primary">About</button></li>
+                          <li><button type="button" className="btn btn-outline-primary">Contribute</button></li>
+                          <li><button type="button" className="btn btn-outline-primary">Contact</button></li>
                         </ul>
                       </div>
                     </div>
@@ -50,40 +49,65 @@ export default function Home() {
 
               <section>
                 <div className="section-discover">
-
                   <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                       <div className="discover">
-                        <p>
-                          Sub Title
-                     <br></br>
-                          <h2>Heading</h2>
-                        </p>
-                        <p className="loram">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio congue egestas ut vitae felis, pellentesque
-                      </p>
+                        <h1>
+                          Discover
+                          FoodieNor
+                        </h1>
+                        <div className="discover-card"  >
+                          <ul className="list-group " >
+                            <li className="list-group-item">Cras justo odio</li>
+                            <li className="list-group-item">Dapibus ac facilisis in</li>
+                            <li className="list-group-item">Vestibulum at eros</li>
+                          </ul>
+                        </div>
 
-                        <button className="button  arrow">Explore</button>
 
                       </div>
                     </div>
-                    <div className="col-md-6">
-                      <div className="discover-card">
-                        <img src="images/FoodieNor logo design .png" alt="food_logo" />
+                    <div className="col-md-5">
+                      <div className="dicover-imagecard card">
+                        <img className="card-img-top" src="http://placeimg.com/640/480/nature" alt="Card image cap"  />
+                        <div className="card-body">
+                          <div className="title" >
+                            <h5 className="card-title ">Blog Heading</h5>
+                            <div className="stars " >
+                              <span className="fa fa-star checked"></span>
+                              <span className="fa fa-star checked"></span>
+                              <span className="fa fa-star checked"></span>
+                              <span className="fa fa-star"></span>
+                              <span className="fa fa-star"></span>
+                            </div>
+                          </div>
+
+                          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
                       </div>
                     </div>
-                    <div className="col-md-2">
-                      <div className="discover-card-buttons">
-                        <ul>
-                          <li><button type="button" className="btn btn-outline-primary">Primary</button></li>
-                          <li><button type="button" className="btn btn-outline-primary">Primary</button></li>
-                          <li><button type="button" className="btn btn-outline-primary">Primary</button></li>
-                        </ul>
+                    <div className="col-md-4">
+                      <div className="card mb-3">
+                        <img className="card-img-top" src="http://placeimg.com/640/480/nature" alt="Card image cap" style={{ height: '214px' }} />
+                        <div className="card-body">
+                          <div className="title" style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <h5 className="card-title ">Card title</h5>
+                            <div className="stars " >
+                              <span className="fa fa-star checked"></span>
+                              <span className="fa fa-star checked"></span>
+                              <span className="fa fa-star checked"></span>
+                              <span className="fa fa-star"></span>
+                              <span className="fa fa-star"></span>
+                            </div>
+                          </div>
+
+                          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
                       </div>
                     </div>
+
                   </div>
-
-
                 </div>
               </section>
             </div>
