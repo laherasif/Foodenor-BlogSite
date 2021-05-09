e397ec9e865c619c5ebef9b991d99f7cafcecd9d
laherasif <laherasif@gmail.com>
2021-05-09 13:38:11 +0500

    All files become to responsive

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,15 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        spaceId: `vyxfcku6ktb1`,
-        // Learn about environment variables: https://gatsby.app/env-vars
-        accessToken: `91ofux4OhB4HYXhjNfA4vZSHgBfEkARtFWc_-yZGpxk`,
-        downloadLocal: true,
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon:`src/images/logo.png`
       },
     },
   ],
--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby'
- import './footer.css'
+ import './styles/footer.css'
+import logo  from '../images/logo.png'
+
 export default function Footer() {
     return (
         <div>
             <footer id="footer" >
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-4 col-md-6 col-sm-7">
+                        <div className="col-lg-4 col-md-6">
                             <div className="footer__about">
                                 <div className="footer__logo" style={{ display: 'flex' }}>
-                                    <img src="images/FoodieNor logo design .png" alt="food_logo" />
+                                    <img src={logo} alt="food_logo" />
                                     <h6 >FOOFIENOR </h6>
                                 </div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                                <p className="paragraph">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 
 
                             </div>
@@ -22,7 +24,7 @@ export default function Footer() {
                         <div className="col-lg-2 col-md-3 col-sm-5">
                             <div className="footer__widget">
                                 <h6>CATAGORIES</h6>
-                                <ul style={{textAlign:'left'}}>
+                                <ul >
                                     <li><Link to="/" className="bottom-links">Home</Link></li>
                                     <li><Link to="/" className="bottom-links">About</Link></li>
                                     <li><Link to="/" className="bottom-links">Blogs</Link></li>
@@ -30,10 +32,10 @@ export default function Footer() {
                                 </ul>
                             </div>
                         </div>
-                        <div className="col-lg-2 col-md-3 col-sm-4">
+                        <div className="col-lg-2 col-md-3 ">
                             <div className="footer__widget">
                                 <h6>Tags Used</h6>
-                                <ul>
+                                <ul >
                                     <li><Link to="/" className="bottom-links">My Account</Link></li>
                                     <li><Link to="/" className="bottom-links">Orders Tracking</Link></li>
                                     <li><Link to="/" className="bottom-links">Checkout</Link></li>
@@ -41,7 +43,7 @@ export default function Footer() {
                                 </ul>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-8 col-sm-8">
+                        <div className="col-lg-4 col-md-8 ">
                             <div id="footer__newslatter">
                                 <h6>NEWSLETTER</h6>
                                 <form  >
