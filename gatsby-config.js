615888377b5b758b72a77a88b0233d3173496ce0
laherasif <laherasif@gmail.com>
2021-05-10 12:41:06 +0500

    changes in netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,46 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon:`src/images/logo.png`
+        icon: `src/images/logo.png`
       },
     },
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/img`,
+        name: 'uploads',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-netlify'
   ],
 }
